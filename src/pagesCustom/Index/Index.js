c88b8234f3e64aa69051ae0f829641bb4aacd716
Ilyes Hermellin <yachaka@gmail.com>
2020-05-11 13:50:14 +0200

    SEO index

--- src/pagesCustom/Index/Index.js
+++ src/pagesCustom/Index/Index.js
@@ -1,10 +1,8 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import { Helmet } from "react-helmet"
 
-// import Layout from "../../components/Layout"
-// import Image from "../components/image"
-// import SEO from "../components/seo"
 import Btn from "../../components/Btn/Btn";
 import Nav from "../../components/Nav/Nav";
 import FacebookPill from '../../components/SocialPills/FacebookPill';
@@ -87,8 +85,32 @@ const IndexPage = () => {
     }
   }
 
+  const title = 'RAWS - Sessions - Production Audiovisuelle, autour de Toulouse';
+  const description = 'RAWS est une boîte de production spécialisée dans la réalisation de vidéos musicales lives, acoustiques et intimistes, le plus souvent tournées en extérieur ou dans des lieux insolites. Afin de correspondre au mieux à leurs attentes et à leurs univers, nous invitons les artistes à participer au processus créatif et au choix du lieu de tournage.';
+  const image = 'https://rawssessions.com' + images.heroBackground.childImageSharp.src;
+
   return (
     <>
+      <Helmet>
+        <meta charSet="utf8" />
+        <title>{title}</title>
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#101010" />
+        <meta name="description" content={description} />
+
+        <meta property="og:url" content="https://rawssessions.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={title} />
+
+        <meta property="og:image" content={image} />
+        <meta property="og:image:alt" content="TO BE FILLED" />
+
+        <meta property="og:description" content={description} />
+        <meta property="og:site_name" content="RAWS" />
+        <meta property="og:locale" content="fr_FR" />
+      </Helmet>
+
       <Nav />
 
       <div id={s.hero}>
