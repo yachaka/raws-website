22194009d92f09c27f70feb7589d62b25818c1a8
Ilyes Hermellin <yachaka@gmail.com>
2020-10-12 17:44:45 +0200

    Ajout Zitoune

--- src/pagesCustom/Index/Index.js
+++ src/pagesCustom/Index/Index.js
@@ -158,7 +158,7 @@ const IndexPage = () => {
           <div id={s.mainExtractVideo}>
             <iframe
               width="100%"
-              src="https://www.youtube.com/embed/EzPY_Ne0erw"
+              src="https://www.youtube.com/embed/R74q_ITO4XU"
               frameborder="0"
               allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
               allowfullscreen
@@ -174,13 +174,33 @@ const IndexPage = () => {
           />*/}
 
           <p id={s.mainExtractDesc}>
-            <span id={s.mainExtractTitle}>Augusta - The Feeling's Gone</span>
+            <span id={s.mainExtractTitle}>Zitoune - The River (Aurora Cover)</span>
             <br />
             {/* Tourné à... (remplacer la ligne complète) */}
           </p>
         </div>
 
         <div className={s.secondaryExtracts}>
+          <div className={s.secondaryExtract}>
+            <div className={s.secondaryExtractVideo}>
+              <iframe
+                width="100%"
+                src="https://www.youtube.com/embed/EzPY_Ne0erw"
+                frameborder="0"
+                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+                allowfullscreen
+              ></iframe>
+            </div>
+
+            <div className={s.secondaryExtractInfos}>
+              <p>
+                <span className={s.secondaryExtractVideoTitle}>Augusta - The Feeling's Gone</span>
+                <br />
+                {/* Tourné à... (remplacer la ligne complète) */}
+              </p>
+            </div>
+          </div>
+
           <div className={s.secondaryExtract}>
             <div className={s.secondaryExtractVideo}>
               <iframe
@@ -200,7 +220,10 @@ const IndexPage = () => {
               </p>
             </div>
           </div>
+        </div>
 
+
+        <div className={s.secondaryExtracts}>
           <div className={s.secondaryExtract}>
             <div className={s.secondaryExtractVideo}>
               <iframe
@@ -221,11 +244,6 @@ const IndexPage = () => {
             </div>
           </div>
 
-          
-        </div>
-
-
-        <div className={s.secondaryExtracts}>
           <div className={s.secondaryExtract}>
             <div className={s.secondaryExtractVideo}>
               <iframe
@@ -241,10 +259,13 @@ const IndexPage = () => {
               <p>
                 <span className={s.secondaryExtractVideoTitle}>David William - Hold Me Down</span>
                 <br />
-               
+
               </p>
             </div>
           </div>
+        </div>
+
+        <div className={s.secondaryExtracts}>
 
           <div className={s.secondaryExtract}>
             <div className={s.secondaryExtractVideo}>
@@ -261,7 +282,7 @@ const IndexPage = () => {
               <p>
                 <span className={s.secondaryExtractVideoTitle}>Augusta - The Birds</span>
                 <br />
-                
+
               </p>
             </div>
           </div>
