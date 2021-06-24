e929d3757b6203989f78273ae27195e2f13e973a
Benji3332 <65251678+Benji3332@users.noreply.github.com>
2021-06-24 12:30:48 +0100

    Update Professionnels.js

--- src/pagesCustom/Professionnels/Professionnels.js
+++ src/pagesCustom/Professionnels/Professionnels.js
@@ -12,7 +12,12 @@ import Nav from "../../components/Nav/Nav";
 
 import s from './Professionnels.module.scss';
 
-const videos = [     
+const videos = [       
+    {
+        url: 'https://www.youtube.com/embed/4a-ZBgMeUo4',
+        title: 'Exposition Nationale d'Elevage | Raws',
+        desc: '',
+    },     
     {
         url: 'https://www.youtube.com/embed/DVWWQ7BB9Uk',
         title: 'Le Passage | Raws',
